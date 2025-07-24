d802a9d6a3b465e488389cdd483cbaa8c340dcbb
Amaanshk06 <amaanshk234@gmail.com>
2025-07-24 20:40:59 +0530

    Added files

---
+++ Script Pract.js
@@ -0,0 +1,188 @@
+// /* 1. Even or Odd*/
+
+// let u = prompt("Enter a number you pro one");
+// if(u %2 ==0){
+//     console.log("Number is even");
+// }else{
+//     console.log("Number is odd");
+// }
+
+
+
+// /* 2. Positive or Negative*/
+
+// let num = prompt("Enter a number");
+// if(num < 0){
+//     console.log("Number is Negative");
+// }
+// else if(num > 0){
+//     console.log("Number is Positive");
+// }else{
+//     console.log("Number is Zero");
+// }
+
+
+
+// /* 3. Print No. from 1 to 10 using for loop*/
+
+// let m =1;
+// for(m; m <= 10; m++){
+//     console.log(m);
+// }
+
+
+
+// /* 4. Sum of First 10 numbers*/
+
+// let sum = 0;
+// for(i=1; i<=10; i++){
+//     sum = sum + i;
+//     console.log(sum);
+// }
+
+
+
+// /* 5. Multiplication Table*/
+// let n = 5;
+// for(i=1; i<=10; i++){
+//     console.log(n,"x",i,"=",n*i);
+// }
+
+
+
+// /* 6. Check Divisibility*/
+// let a = prompt("Enter a number");
+// if(a %3 == 0 && a %5 == 0){
+//    console.log("Divisible by both");
+   
+// } else if(a %3 == 0){
+//    console.log("Only by 3");
+   
+// }else if(a %5 == 0){
+//     console.log("only by 5");
+    
+// }else{
+//     console.log("Not divisible");
+    
+// }
+
+
+// /* 7. Factorial of a Number*/
+// let mul = 1, n = 0;
+// for(i = 1; i <= n; i++){
+//     mul = mul * i;
+// }
+//  console.log(mul);
+
+
+
+// /*  8. Sum of even numbers*/
+// let sum = 0;
+// for(let n=1; n<=20; n++){
+//     if(n %2==0){
+//        sum = sum + n;
+//     }
+    
+// }console.log(sum);
+
+
+
+// /* 9. Reverse Counting*/
+// let d = 10;
+// for(d; d>0; d--){
+// console.log(d);
+// } 
+
+
+
+// /* Largest of two numbers*/
+// let number1 = prompt("Enter a  First number"); 
+// let number2 = prompt("Enter a  Second number"); 
+// if(number1>number2){
+//     console.log("Number1 is Greater");
+    
+// }else{
+//     console.log("Number2 is Greater");
+    
+// }
+
+
+
+/* Questions
+1- If number is multiple of 7.
+2- Print Squares of number from 1 to 10.
+3- print first 10 multiples of any number n.
+4- Print numbers from 50 to 40 in reverse order.
+5- Check if number is positive and even.
+6- print sum of odd number from 1 to 100.
+7- Count how many number between 1 to 100 are divisible by 3.
+*/
+
+
+/* Answer 1*/ 
+let num = 49;
+if(num %7 == 0){
+    console.log("Yes, Number is divisible by 7");
+    
+}else{
+    console.log("No, Number is not divisible by 7");
+    
+}
+
+// /* Ans 2*/
+// let number = 1;
+// for(number;number<=10;number++){
+//     console.log(number*number);
+    
+// }
+
+/* Ans 3*/
+// let m = 2;
+// for(n = 1; n<=10; n++){
+//     console.log(m,"x",n,"=",n*m);
+    
+// }
+
+/* Ans 4*/
+// let a = 50;
+// for(a; a>=40; a--){
+   
+//     console.log(a);
+// } 
+
+/* Ans 5*/
+// let p = prompt("Enter a number");
+// if(p > 0){
+//     if(p %2 == 0){
+//         console.log("Yes, number is  positive and also even");
+        
+//     }else{
+//         console.log("Number is not even but positive ");
+        
+//     }
+// }else{
+//     console.log("No,Number is not positive ");
+    
+// }
+
+/* Ans 6*/
+// let sum= 0;
+// for(let b = 1; b<=100;b++){
+//     if(b %2 ==0){
+
+   
+//     }else{
+//          sum = sum + b;
+//         console.log(sum);
+//     }
+// }
+
+/* Ans 7*/
+// let count = 0;
+// for(let num = 1; num<=100;num++){
+//     if(num%3==0){
+//         count++
+//     }
+// }
+// console.log(count);
+
---
+++ script.js
@@ -0,0 +1,63 @@
+// /* SWAPPING THE VALUES */
+// let number1 = 300;
+// let number2 = 400;
+// console.log("Before swapping values are:",number1,number2);
+// temp = number1;
+// number1 = number2;
+// number2 = temp;
+// console.log("After swapping values are:",number1,number2);
+
+// /* Leap year code */
+// let year = prompt("Enter a year");
+// if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
+//     console.log("Yes, It's a Leap year");
+// }else{
+//     console.log("Not a leap year");
+// }
+
+// /* For loop */
+// let a = 1;
+// for(a ; a < 101; a++) {
+//     console.log("Hello World",a);
+// }
+// /*Table of number*/
+// let n = 10;
+// for(i=1; i<=10;i++){
+//     console.log(n,"x",i,"=",n*i);
+// }
+// let sum = 0;
+// for(i=1; i<=10; i++){
+//     sum=sum+i
+//     console.log(sum);
+// }
+// console.log(sum);
+
+// /* Pre,Post:Increment and Decrement*/
+// let i = 10;
+// for(i; i>=1;i--){
+//     console.log(i);
+// }
+
+// /*Even and Odd*/
+// let num = 0;
+// console.log("Even numbers from 0 to 10 are:");
+// for(; num<=10;num+=2){
+//     console.log(num);
+// }
+// console.log("Odd numbers from 0 to 10 are:");
+// let num = 0;
+// for(;num<=10;num++){
+//     if(num%2==1){
+//         console.log(num);
+//     }
+
+// }
+
+// let m = 10;
+// let n = ++m;
+// console.log(m);
+// console.log(n);
+
+// let a = 2;
+// let result = a++ + 3;
+// console.log(result);
